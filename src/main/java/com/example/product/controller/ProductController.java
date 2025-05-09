1e04de182b2a6736a2720c926b29efdbbcc1e8a9
Saget <132357928+Saget-a@users.noreply.github.com>
2025-05-09 12:30:09 +0300

    --

--- src/main/java/com/example/product/controller/ProductController.java
+++ src/main/java/com/example/product/controller/ProductController.java
@@ -1,15 +1,17 @@
 package com.example.product.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.example.product.client.AuthClient;
 import com.example.product.model.Product;
 import com.example.product.service.ProductService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
